b022216a8e7cd60484f5338c02bd7ad721ae3fcd
jeongeum1202 <155497479+jeongeum1202@users.noreply.github.com>
2024-02-15 15:45:13 +0900

    Update CustomQuillEditor.tsx

--- src/components/common/editor/CustomQuillEditor.tsx
+++ src/components/common/editor/CustomQuillEditor.tsx
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic';
-import React, { useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import ReactQuill from 'react-quill';
 import * as S from './styled';
 import CustomToolbar from './CustomToolBar';
@@ -26,6 +26,7 @@ const QuillWrapper = dynamic(
 
 export default function CustomQuillEditor() {
   const [contents, setContents] = useState('');
+  const [viewContent, setViewContent] = useState('');
   const quillRef = useRef<ReactQuill>(null);
   const modules = useMemo(() => {
     return {
@@ -35,7 +36,23 @@ export default function CustomQuillEditor() {
       },
     };
   }, []);
+  const handleChange = (contents: any) => {
+    setContents(contents);
+    console.log('contents:', contents);
+    console.log('view:', viewContent);
+  };
 
+  const parseString = (html: any) => {
+    console.log(html);
+    // DOM Parser를 생성합니다. 문자열로 된 데이터를 DOM Document로 변환합니다.
+    const doc = new DOMParser().parseFromString(html, 'text/html');
+    // parsed document에서 textContent를 사용하여 순수 텍스트만을 추출합니다.
+    return doc.body.innerHTML;
+  };
+
+  useEffect(() => {
+    setViewContent(() => contents.replace(/<(?!br\s*\/?>)[^>]*>/g, ''));
+  }, [contents]);
   const formats = [
     'font',
     'size',
@@ -53,16 +70,29 @@ export default function CustomQuillEditor() {
     'image',
   ];
   return (
-    <S.QuillWrapper>
-      <CustomToolbar />
-      <QuillWrapper
-        modules={modules}
-        formats={formats}
-        value={contents}
-        theme="snow"
-        forwardedRef={quillRef}
-        placeholder="내용을 입력해주세요."
-      />
-    </S.QuillWrapper>
+    <>
+      <S.QuillWrapper>
+        <CustomToolbar />
+        <QuillWrapper
+          onChange={handleChange}
+          modules={modules}
+          formats={formats}
+          value={contents}
+          theme="snow"
+          forwardedRef={quillRef}
+          placeholder="내용을 입력해주세요."
+        />
+      </S.QuillWrapper>
+      <div>
+        <p>원본 HTML 내용:</p>
+        <div dangerouslySetInnerHTML={{ __html: parseString(contents) }}></div>
+        <p>텍스트만 추출한 내용:</p>
+        <div>
+          {parseString(
+            '<p>안녕하세요!</p><p><br></p><p>줄바꿈을 해볼게요</p><p>한줄은 안되더라고요</p><p><br></p><p>두줄을 해줘야해요</p><p><br></p><p><strong><em><u>글씨스타일 바꿔볼게요</u></em></strong></p><p><br></p><p><strong><em><u>이렇게해도 되는건가?!?!</u></em></strong></p><p>해봅시다.</p>'
+          )}
+        </div>
+      </div>
+    </>
   );
 }
